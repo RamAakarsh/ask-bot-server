9d7165f92c36d76d2e86985702d55549c2821f35
Ram Aakarsh Iruvanti <aakarsh1206@gmail.com>
2026-02-01 22:02:53 -0500

    Update httpConfigurator.js

--- src/modules/httpConfigurator.js
+++ src/modules/httpConfigurator.js
@@ -7,7 +7,7 @@ const onListening = (port) => {
 };
 
 module.exports.configure = (app, config) => {
-	const port = 8080;//_.isEmpty(process.env.PORT) ? config.get('http.port') : process.env.PORT;
+	const port = _.isEmpty(process.env.PORT) ? config.get('http.port') : parseInt(process.env.PORT);
 	const server = app.listen(port, onListening(port));
 	socketServerConfig(server)
     connectWithOpenAi();
